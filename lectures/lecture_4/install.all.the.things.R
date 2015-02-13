094babca66b4775634287b143168aba32c709aad
chrishwiggins <chris@hackny.org>
2015-02-13 07:36:16 -0500

    library installation script

---
+++ lectures/lecture_4/install.all.the.things.R
@@ -0,0 +1,8 @@
+# 6 libraries we will use today:
+
+install.packages("glmnet", verbose=TRUE, dependencies = TRUE, repos="http://cran.us.r-project.org")
+install.packages("ggplot2", verbose=TRUE, dependencies = TRUE, repos="http://cran.us.r-project.org")
+install.packages("dplyr", verbose=TRUE, dependencies = TRUE, repos="http://cran.us.r-project.org")
+install.packages("ggplot2", verbose=TRUE, dependencies = TRUE, repos="http://cran.us.r-project.org")
+install.packages("scales", verbose=TRUE, dependencies = TRUE, repos="http://cran.us.r-project.org")
+install.packages("reshape", verbose=TRUE, dependencies = TRUE, repos="http://cran.us.r-project.org")
