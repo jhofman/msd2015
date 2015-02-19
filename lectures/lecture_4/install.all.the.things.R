2a1c63639cfd63e996be0b5846d5d07b623f9bea
chrishwiggins <chris@hackny.org>
2015-02-19 05:53:54 -0500

    took out duplicate ggplot2

--- lectures/lecture_4/install.all.the.things.R
+++ lectures/lecture_4/install.all.the.things.R
@@ -3,6 +3,5 @@
 install.packages("glmnet", verbose=TRUE, dependencies = TRUE, repos="http://cran.us.r-project.org")
 install.packages("ggplot2", verbose=TRUE, dependencies = TRUE, repos="http://cran.us.r-project.org")
 install.packages("dplyr", verbose=TRUE, dependencies = TRUE, repos="http://cran.us.r-project.org")
-install.packages("ggplot2", verbose=TRUE, dependencies = TRUE, repos="http://cran.us.r-project.org")
 install.packages("scales", verbose=TRUE, dependencies = TRUE, repos="http://cran.us.r-project.org")
 install.packages("reshape", verbose=TRUE, dependencies = TRUE, repos="http://cran.us.r-project.org")
