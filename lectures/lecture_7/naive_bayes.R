bd8aa736598364624803076aa856d7af4eee995b
Jake Hofman <jhofman@gmail.com>
2015-03-13 08:03:21 -0400

    fix to naive bayes in R

--- lectures/lecture_7/naive_bayes.R
+++ lectures/lecture_7/naive_bayes.R
@@ -3,24 +3,26 @@ library(ElemStatLearn)
 library(e1071)
 library(ROCR)
 
+# create train/text indices
 ndx <- sample(nrow(spam), floor(nrow(spam) * 0.9))
 train <- spam[ndx,]
 test <- spam[-ndx,]
 
+# split into train/test data
 xTrain <- train[,-58]
 yTrain <- train$spam
-
 xTest <- test[,-58]
 yTest <- test$spam
 
+# fit naive bayes model with default params
 model <- naiveBayes(xTrain, yTrain)
 
+# look at confusion matrix
 table(predict(model, xTest), yTest)
 
+# plot ROC
 probs <- predict(model, xTest, type="raw")
-probs <- transform(probs, p_spam=ifelse(spam > email, spam, 1-spam))
-
-pred <- prediction(probs$p_spam, yTest)
+pred <- prediction(probs$spam, yTest)
 perf <- performance(pred, measure='tpr', x.measure='fpr')
 plot(perf)
 performance(pred, 'auc')
